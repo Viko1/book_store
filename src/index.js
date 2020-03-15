531d58cbb42f1cd6d61ea0e33edf70cf63593382
Viko1 <vitaliiropiak@gmail.com>
2020-03-15 22:58:25 +0100

    Books store V 0.01

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Provider} from 'react-redux';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import createStore from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+setTimeout(function () {
+    store.dispatch({
+        type: 'SET_BOOKS',
+        payload: [
+            {
+                id: 0,
+                title: 'Hello World'
+            }
+        ]
+    });
+}, 1000);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root'));
+
---
+++ src/reducers/books.js
@@ -0,0 +1,22 @@
+const initialState = {
+    books: [],
+};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_BOOKS':
+            return {
+                ...state,
+                books: action.payload
+            };
+        case 'ADD_BOOKS':
+            return{
+                books: [
+                    ...state.books,
+                    action.payload
+                ]
+            };
+        default:
+            return state;
+    }
+}
